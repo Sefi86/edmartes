c68778adf45c4d5a55745be050fa7f21c8749d55
Sefi86 <pepiadalid86@gmail.com>
2015-05-05 12:47:03 +0200

    2

--- trabajoEDMartes/src/trabajoEDMartes/holaa2.java
+++ trabajoEDMartes/src/trabajoEDMartes/holaa2.java
@@ -1,1 +1,16 @@
-syso
+
+package trabajoEDMartes;
+
+public class HolaMundo {
+
+	public static void main(String[] args) {
+		System.out.println("holaaaa");
+		
+		System.out.println("ahora siii");
+		System.out.println(":D");
+		
+		
+
+	}
+
+}
