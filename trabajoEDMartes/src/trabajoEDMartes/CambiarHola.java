244035a5effff059a7c80babe4894378f53534f6
Sefi86 <pepiadalid86@gmail.com>
2015-05-07 13:43:24 +0200

    1

--- trabajoEDMartes/src/trabajoEDMartes/CambiarHola.java
+++ trabajoEDMartes/src/trabajoEDMartes/CambiarHola.java
@@ -3,8 +3,7 @@ package trabajoEDMartes;
 public class CambiarHola {
 
 	public static void main(String[] args) {
-		System.out.println("siip");
-		System.out.println("modificando");
+	System.out.println("operaciones"+ (1+1));
 
 	}
 
