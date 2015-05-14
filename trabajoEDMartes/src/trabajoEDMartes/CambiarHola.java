04d73bc1683d22f91e86319a433b47a1ec7083cc
martamorcillocarrasco <martamorcillocarrasco@alumno-desktop>
2015-05-14 11:42:02 +0200

    pepii

--- trabajoEDMartes/src/trabajoEDMartes/CambiarHola.java
+++ trabajoEDMartes/src/trabajoEDMartes/CambiarHola.java
@@ -4,6 +4,7 @@ public class CambiarHola {
 
 	public static void main(String[] args) {
 	System.out.println("operaciones"+ (1+1));
+	syso
 
 	}
 
--- trabajoEDMartes/src/trabajoEDMartes/Modificaciones.java
+++ trabajoEDMartes/src/trabajoEDMartes/Modificaciones.java
@@ -5,6 +5,9 @@ public class Hola4 {
 	public static void main(String[] args) {
 		System.out.println("siip");
 
+		System.out.println("martaaa");
+	
+		
 	}
 
 }
