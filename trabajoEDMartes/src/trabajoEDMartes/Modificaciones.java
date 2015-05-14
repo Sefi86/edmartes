749a452f61263c05b784273950e5781ac9335844
Sefi86 <pepiadalid86@gmail.com>
2015-05-14 11:46:36 +0200

    He insertado una frase más

--- trabajoEDMartes/src/trabajoEDMartes/Modificaciones.java
+++ trabajoEDMartes/src/trabajoEDMartes/Modificaciones.java
@@ -3,6 +3,9 @@ package trabajoEDMartes;
 public class Hola4 {
 
 	public static void main(String[] args) {
+		
+		
+		System.out.println("nuevos cambios en la clase");
 		System.out.println("siip");
 
 		System.out.println("martaaa");
