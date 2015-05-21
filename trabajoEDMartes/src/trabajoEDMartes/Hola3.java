ba0736cb7f53b2230cf80c04052ef5f584313304
Sefi86 <https://github.com/Sefi86/TrabajoED.git>
2015-05-21 19:37:42 +0200

    cuenta

--- trabajoEDMartes/src/trabajoEDMartes/Hola3.java
+++ trabajoEDMartes/src/trabajoEDMartes/Hola3.java
@@ -7,8 +7,10 @@ public class Hola3 {
 		System.out.println("definitiva");
 		System.out.println("Prueba 1");
 		System.out.println("Prueba 2");
-		System.out.println("Prueba 2");
-		System.out.println("Prueba 2");
+		System.out.println("Prueba 3");
+		System.out.println("Prueba 4");
+		System.out.println("Prueba 5");
+		System.out.println("Prueba 6");
 	}
 
 }
